a26060aa841033f399782417dfdd25b7d2500b99
James Coleby <32794776+decoleby91@users.noreply.github.com>
2022-12-22 22:37:19 +0000

    Adding the JavaScript in order to build the quiz

---
+++ js/hiragana_test.js
@@ -0,0 +1,38 @@
+/*quiz data */
+const quizData = [
+    {
+      question: 'Who is the current CEO of Google ?',
+      a: 'Sunder Pichai',
+      b: 'Larry Page ',
+      c: 'Jhon Brown',
+      correct: 'a',
+    },
+    {
+      question: 'Who is the current CEO of Amazon ?',
+      a: 'Jeff Bezos',
+      b: 'Warren Buffet ',
+      c: 'Andy Jassy',
+      correct: 'c',
+    },
+    {
+      question: 'Who is the current CEO of Tesla ?',
+      a: 'Jeffry Black',
+      b: 'Elon Musk ',
+      c: 'Jhon Brown',
+      correct: 'b',
+    },
+    {
+      question: 'Who is the current CEO of Microsoft ?',
+      a: 'Satya Nadella ',
+      b: 'Tom Klington',
+      c: 'Jhon Brown',
+      correct: 'a',
+    },
+    {
+      question: 'Which of the company is owned by Mark Zkerburg ?',
+      a: 'Neuralink ',
+      b: 'Meta Platforms Inc ',
+      c: 'Metaverse LLC ',
+      correct: 'b',
+    },
+  ];
