8a5707937ec69cc143b930f6f4329ea7de97fe4c
James Coleby <32794776+decoleby91@users.noreply.github.com>
2022-12-22 22:45:29 +0000

    Defined the quiz logic

--- js/hiragana_test.js
+++ js/hiragana_test.js
@@ -44,3 +44,50 @@ const b = document.getElementById('b+');
 const c = document.getElementById('c+');
 const btn = document.getElementById('submit');
 const all_answer = document.querySelectorAll('.answer');
+
+let index = 0;
+let score = 0;
+//get the getSelected answer
+function getSelected() {
+  let ans = undefined;
+  all_answer.forEach((el) => {
+    if (el.checked) {
+      ans = el.id;
+    }
+  });
+  return ans;
+}
+//disselect all answer
+function deselectans() {
+  all_answer.forEach((el) => {
+    el.checked = false;
+  });
+}
+//load the quiz data
+function getquiz() {
+  deselectans();
+  question.innerText = quizData[index].question;
+  a.innerText = quizData[index].a;
+  b.innerText = quizData[index].b;
+  c.innerText = quizData[index].c;
+}
+//move forward the quiz
+function startquiz() {
+  btn.addEventListener('click', () => {
+    let ans = getSelected();
+    if (ans) {
+      if (ans == quizData[index].correct) {
+        score++;
+      }
+    }
+    index++;
+    if (index < quizData.length) {
+      getquiz();
+    } else {
+      alert('score :' + score);
+      location.reload();
+    }
+  });
+}
+getquiz();
+startquiz();
